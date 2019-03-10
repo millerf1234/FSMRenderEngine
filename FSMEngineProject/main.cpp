9bb95fb61fbe4631919b25736e72ab08e9c1c357
Forrest S Miller <4estmiller@comcast.net>
2019-03-09 19:34:42 -0800

    Added a small test to main() to confirm that delegating constructors work as I expect them to

--- FSMEngineProject/main.cpp
+++ FSMEngineProject/main.cpp
@@ -8,7 +8,29 @@
 #include <memory>
 #include "Application.h"
 
+class DelegateConstructorTest {
+public:
+    DelegateConstructorTest() : DelegateConstructorTest(5) {
+        throw "Error!\n";
+    }
+    DelegateConstructorTest(int x) {
+        mX_ = x;
+    }
+    ~DelegateConstructorTest() {
+        printf("Destructor called!\n");
+    }
+
+    int mX_;
+};
+
+
 int main(int argc, char ** argv) {
+    try {
+        DelegateConstructorTest t;
+    }
+    catch (...) {
+        printf("\nCaught it tho!\n");
+    }
     std::unique_ptr<Application> app = std::make_unique<Application>(argc, argv);
     app->launch();
 }
